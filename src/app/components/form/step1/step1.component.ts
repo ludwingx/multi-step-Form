0ae55fb97ef39c5f756e743e3efad19d9e22378e
ludwingx <ludwingytx@gmail.com>
2023-12-31 21:48:08 -0400

    0.0.1

---
+++ src/app/components/form/step1/step1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step1',
+  standalone: true,
+  imports: [],
+  templateUrl: './step1.component.html',
+  styleUrl: './step1.component.scss'
+})
+export class Step1Component {
+
+}
---
+++ src/app/components/form/step4/step4.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step4',
+  standalone: true,
+  imports: [],
+  templateUrl: './step4.component.html',
+  styleUrl: './step4.component.scss'
+})
+export class Step4Component {
+
+}
