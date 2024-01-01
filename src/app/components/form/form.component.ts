ddeed83963de07cc6a52327971669988be457da5
ludwingx <ludwingytx@gmail.com>
2023-12-31 22:35:57 -0400

    0.0.1

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Step1Component} from './step1/step1.component';
+import { Step2Component} from './step2/step2.component';
+import { Step3Component} from './step3/step3.component';
+import { Step4Component} from './step4/step4.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [Step1Component, Step2Component, Step3Component, Step4Component],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
